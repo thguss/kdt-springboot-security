45d9ef0e688190d1dbfe47ba0af0b2ac27212358
김소현 <thgus345@naver.com>
2022-12-12 17:45:46 +0900

    [DAY1] Spring Security Quickstart

---
+++ src/main/java/com/progrms/devcource/configures/WebSecurityConfigure.java
@@ -0,0 +1,33 @@
+package com.progrms.devcource.configures;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigure {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/assets/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/me").hasAnyRole("USER", "ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and();
+
+        return http.build();
+    }
+}
