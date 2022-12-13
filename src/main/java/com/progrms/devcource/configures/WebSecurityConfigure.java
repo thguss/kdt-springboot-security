04efa89f7873935d17bd1cb56c7a4da84fe8bcb4
김소현 <thgus345@naver.com>
2022-12-13 18:34:05 +0900

    [DAY2] DEL: 주석 삭제

--- src/main/java/com/progrms/devcource/configures/WebSecurityConfigure.java
+++ src/main/java/com/progrms/devcource/configures/WebSecurityConfigure.java
@@ -8,9 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,7 +24,6 @@ public class WebSecurityConfigure {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // .requestMatchers("/me")
         http
                 .authorizeHttpRequests()
                 .antMatchers("/me")
