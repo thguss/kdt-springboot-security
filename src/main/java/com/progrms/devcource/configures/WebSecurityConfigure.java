0b1168bbdc198770b0d5706b46d0fea1481c253c
김소현 <thgus345@naver.com>
2022-12-20 16:17:21 +0900

    [DAY6] 데이터베이스 기반 고급 인증 설정

--- src/main/java/com/progrms/devcource/configures/WebSecurityConfigure.java
+++ src/main/java/com/progrms/devcource/configures/WebSecurityConfigure.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
@@ -174,12 +175,32 @@ public class WebSecurityConfigure {
     public UserDetailsService userDetailsService(DataSource dataSource) {
         JdbcDaoImpl jdbcDao = new JdbcDaoImpl();
         jdbcDao.setDataSource(dataSource);
+        jdbcDao.setEnableAuthorities(false);
+        jdbcDao.setEnableGroups(true);
+        jdbcDao.setUsersByUsernameQuery(
+                "SELECT " +
+                    "login_id, passwd, true " +
+                "FROM " +
+                    "USERS " +
+                "WHERE " +
+                    "login_id = ?"
+        );
+        jdbcDao.setGroupAuthoritiesByUsernameQuery(
+                "SELECT " +
+                    "u.login_id, g.name, p.name " +
+                "FROM " +
+                    "users u JOIN groups g ON u.group_id = g.id " +
+                        "LEFT JOIN group_permission gp ON g.id = gp.group_id " +
+                        "JOIN permissions p ON p.id = gp.permission_id " +
+                "WHERE " +
+                    "u.login_id = ?"
+        );
+
         return jdbcDao;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
-
 }
